d843565b5653ed9fcdb2c331e8f1903ba5d02dfb
Daniel Wesson <daniil.j@icloud.com>
2019-11-18 22:22:29 +0300

    Deploy

--- app/Http/Controllers/Catalog/CollectionController.php
+++ app/Http/Controllers/Catalog/CollectionController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class CollectionController extends \App\Http\Controllers\SiteController
 {
-    public function show(Request $request) {
+    public function show(Request $request, $collection_id) {
     	$this->template = 'catalog.collection';
     	$this->title = 'Коллекция - Sneakerdark';
     	return $this->output();
